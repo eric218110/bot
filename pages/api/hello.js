013d39f117314e2c430c529623497de10296541d
Eric Silva <ericsilvaccp@gmail.com>
2022-07-14 16:12:39 -0300

    teste

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,34 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import puppeteer from "puppeteer";
 
-export default function handler(req, res) {
+export default async function handler(req, res) {
+  const usernameBlaze = 'blazeappbottest1995@gmail.com'
+  const passwordBlaze = 'Wghd$85a'
+
+  const baseSite = "https://blaze.com/pt/"
+  const loginPage = 'https://blaze.com/pt/?modal=auth&tab=login'
+  const gameCrashUrl = 'https://blaze.com/pt/games/crash'
+
+  const nameInputUserName = 'username'
+  const nameInputPassword = 'password'
+
+  const browser = await puppeteer.launch({
+    headless: false,
+  });
+
+  const page = await browser.newPage();
+  await page.goto(loginPage);
+
+  await page.type(`input[name=${nameInputUserName}]`, usernameBlaze)
+  await page.type(`input[name=${nameInputPassword}]`, passwordBlaze)
+  await page.keyboard.press('Enter')
+
+  await page.waitForNavigation()
+
+  await page.goto(gameCrashUrl)
+
+  console.log("Login realizado com sucesso")
+  browser.close()
+
   res.status(200).json({ name: 'John Doe' })
 }
